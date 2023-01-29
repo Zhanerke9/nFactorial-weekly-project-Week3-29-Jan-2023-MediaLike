52dfbc363f8381f5bb8aa07f6e3198a6f39761ce
Zhanerke9 <zhanerke.sauranbay@gmail.com>
2023-01-29 17:32:22 +0500

    nFactorial-weekly-project-Week3-29-Jan-2023-MediaLike

---
+++ mainJSfile.js
@@ -0,0 +1,310 @@
+// const articlesFromAPI = [
+//     {
+//       id: 0,
+//       summary:
+//          `"Many gin connoisseurs insist that the best juniper comes from Macedonia; Hapusa argues otherwise, relying on larger juniper berries foraged from the Himalayas along with other native ingredients, including turmeric, raw mango and ginger.
+
+// When Nao Spirits first produced a small-batch gin from Goa in 2017, partnering with a local bottler to get a state permit, no one seemed to have made the connection between India, the cradle of the spice trade for centuries, and gin, which is really just a blank canvas of alcohol with herbs and other plants used to brighten the portrait.
+
+// Only a few years later, Goan gin has become a bountiful and colorful bouquet. Pumori gin — named after Mount Pumori, a few miles west of Mount Everest — also uses Himalayan juniper, along with other local botanicals. Jin Jiji relies on Indian juniper, adding a Goan specialty, cashew nuts, while Matinee gin includes Goan peppercorn. Samsara and GinGin also use native Indian ingredients, including a touch of hemp."`,
+//         src: "./images/Img1.png",
+//     },
+
+//     {
+//       id: 1,
+//       summary: `SYDNEY — Authorities in Western Australia are searching for a dangerously radioactive capsule, which they believe fell off a truck while being transported.
+
+// But they have a problem: The capsule is smaller than a penny, while the search zone is a stretch of vast desert highway about as long as California’s coastline.
+
+// The capsule, a small silver cylinder measuring 0.3 inches by 0.2 inches, came from a Rio Tinto mine and formed a part of a sensor used in mining. The sensor had been put onto a truck and driven from the mine, near Newman in the remote north of Western Australia, to Perth, the state’s capital, on a trip that took several days.
+
+// The capsule, which contains a small amount of cesium-137, is dangerously radioactive, according to authorities. An hour of exposure at about a meter (about 3 feet) away is the equivalent of having 10 X-rays, and prolonged contact can cause skin burns, acute radiation sickness and cancer, they said.
+
+// The truck carrying the sensor arrived in Perth on Jan. 16. On Friday, nearly two weeks later, authorities called an emergency news conference to alert the public that the capsule had disappeared somewhere along the 1,400-kilometer (870-mile) journey.
+
+// “We want the public to be alert to the possibility of finding the small capsule and the risks,” David Gill, a chief superintendent for the Western Australia emergency services department, said at a news conference Friday.
+
+// The state’s chief health officer, Dr. Andrew Robertson, warned the public to stay at least 5 meters away from the capsule if they spot it.
+
+// Authorities have acknowledged that they have a vast distance to cover and that the search is likely to take “weeks, not days.” But they hope it will be a little easier than looking for a needle in a haystack.
+
+// “What we are not doing is trying to find a tiny little device by eyesight,” Darryl Ray, the acting superintendent for Western Australia’s Department of Fire and Emergency Services, said at a news conference Saturday. “We are using the radiation detectors to locate the gamma rays, using the meters, that will help us then locate the small device.”
+
+// The news of a misplaced radioactive cylinder had many on the internet making the joke: Is Homer Simpson to blame?
+
+// The capsule was not lost through cartoonish buffoonery but through an almost cinematic series of events.
+
+// According to authorities, the sensor containing the capsule was placed inside a wooden box, which was screwed onto a pallet and placed in the back of a flatbed truck.
+
+// They believe that vibrations from the truck caused the sensor to shake apart and also dislodged a mounting bolt, leaving a hole in the bottom of the box. The capsule is believed to have fallen out of the sensor, through the bolt-hole, onto the surface of the truck, and bounced off into the road.
+
+// Although the truck made the journey from Jan. 12 to Jan. 16, the capsule was not discovered to be missing until Wednesday. The box that the sensor was stored in was “completely secure on arrival” in Perth, where it was put into protective storage, Robertson, the public health official, said Friday. It was only when the box was unpacked for inspection for the first time Wednesday that the capsule was discovered missing, he added.
+
+// When the box was opened, it was discovered that the sensor had “broken apart,” Robertson told a local radio station. “It was literally in pieces.”`,
+//           src: "./images/Img2.png",
+//     },
+//     {
+//       id: 2,
+//       title: "10 Practical JS Tips",
+//       summary: `JERUSALEM — The new far-right government in Israel has been in power for only a month, but on its watch, Israelis and Palestinians have already experienced one of their region’s most violent phases, outside a full-scale war, in years.
+
+// Nine Palestinians were shot dead Thursday morning, in the deadliest Israeli raid in the West Bank for at least a half-decade. Then, a Palestinian gunman killed seven people Friday night outside a synagogue in Jerusalem, the deadliest attack on civilians in the city since 2008. And Saturday, an attacker who police said was 13 years old shot and injured two Israelis near a settlement in east Jerusalem.
+
+// These events were not unique to this government’s tenure. But analysts fear that the policies and leaders of the new Israeli administration — the most right-wing in Israeli history — are likely to further inflame the situation.
+
+// The new government is an alliance of settler activists, hard-line nationalists and ultraconservatives helmed by Benjamin Netanyahu, and its leaders variously seek to annex the West Bank, further ease the Israeli army’s rules of engagement and entrench Israeli control over a sacred site in Jerusalem. All of that has already provoked a surge in Palestinian anger and made it harder for the remaining moderate forces in the Israeli government to defuse tensions.
+
+// Under the previous government, “Israeli policy was designed to maintain the illusion of stability,” said Nimrod Novik, a former senior Israeli official and an analyst at the Israel Policy Forum.
+
+// Now, Novik added, “That cover has been taken away.”
+
+// In recent interviews, Netanyahu has frequently dismissed such arguments as alarmist and said that his party, Likud, would take charge of maintaining stability.
+
+// Military strategy is about “deciding on policies that could be quite inflammatory,” he said last month in a podcast interview. “I’m trying to avoid that,” he added.
+
+// To be sure, the government has also inherited an unstable dynamic from previous administrations.
+
+// The Jerusalem shooting drew comparisons with a wave of five Palestinian attacks that killed 19 Israelis and foreigners last spring, during the previous Israeli government’s tenure.
+
+// The West Bank raid was simply the continuation of a 10-month Israeli military campaign in the territory that the previous government began in response to that wave of violence last spring and that led to the deaths of more than 170 Palestinians in 2022, the highest annual toll in the West Bank for more than a decade and a half. Thirty Israelis and foreigners were killed last year by Palestinians, the highest toll since 2014.
+
+// The long-term roots of this cycle — including Israel’s occupation of the West Bank in 1967 and the establishment of a two-tier legal system there for Israeli settlers and Palestinians; the failure of peace negotiations, which stalled in 2014; and Palestinian rejection of Israel and violence against Israelis — also far predate any contemporary Israeli government.
+
+// Nevertheless, extremists in the current government were elected on promises that have already added fuel to Palestinian anger. And they have proved emboldened, not cowed, by the rise in tensions.
+
+// Itamar Ben-Gvir, the new minister in charge of police, won a record number of seats in the general election in November after campaigning to take stronger action against Palestinians he deems a terrorist threat and playing off fears exacerbated by interethnic riots between Arabs and Jews in 2021.
+
+// The attack in Jerusalem on Friday has already heightened calls from his supporters to make good on his promises.
+
+// “Itamar, deal with them, Itamar!” shouted one bystander after Ben-Gvir arrived at the scene of the attack. “We elected you, Itamar.”
+
+// “The government needs to respond,” Ben-Gvir replied. “With God’s help, I hope that that’s what happens.”
+
+// Ben-Gvir did not give details, but his background has made Palestinians particularly apprehensive of his next steps. In the 1990s, he was barred from serving in the Israeli army because security officials deemed him too extremist. Until 2020, he displayed a large portrait in his home of a Jewish gunman who killed 29 Palestinians in a West Bank mosque in 1994.
+
+// “There’s a major change here,” said Hani Masri, a Ramallah, West Bank-based political analyst. “We used to see this on the fringes, not among ministers.
+
+// “We are in a new stage,” he added.
+
+// The new Israeli government has already prompted greater focus on whether the two-state solution — the term for a peace deal that would create a Palestinian state alongside Israel — is not just unlikely but impossible. The government’s statement of guiding principles began with a direct assertion of the Jewish people’s exclusive right to both Israel and the occupied West Bank.
+
+// Another coalition agreement promised to formally annex the West Bank at a time of Netanyahu’s choice and to legalize dozens of unauthorized settlements in the territory.
+
+// For now, Netanyahu has restrained some of his more hard-line ministers from fully exerting their will in the West Bank.
+
+// This month, he ignored demands from Bezalel Smotrich, a far-right minister, to stop the army from evicting an unauthorized Israeli settlement in the territory. But it is unclear how long he can continue to deny his coalition partner: He has promised to give Smotrich power over the military department that oversees construction and demolition in Israeli-administered parts of the territory.
+
+// Through public and private interventions, the United States has also tried to avert some ministers’ more drastic goals in the West Bank. Secretary of State Antony Blinken is set to visit Jerusalem and Ramallah on Monday and Tuesday, in a long-planned visit.
+
+// But analysts doubt much can be achieved, given the high emotions in Israel and the West Bank after this past week’s events.
+
+
+// The visit is “more likely to resemble an extended condolence call than a productive diplomatic mission,” said Aaron Miller, a former U.S. diplomat and a fellow at the Carnegie Endowment for International Peace, a Washington-based research group.
+
+// “The blood is up on both sides,” he added.
+
+
+// Internal divisions within Palestinian society and its leadership will also impede efforts to salve the situation. The Palestinian Authority, the semi-autonomous body that has administered most Palestinian towns and cities in the West Bank since the 1990s, is deeply unpopular among ordinary Palestinians, many of whom accuse it of collaboration with Israel for coordinating with Israeli security forces.
+
+// Since the authority’s establishment, its police and intelligence officers have cooperated with Israeli counterparts, sharing intelligence that officials say has helped thwart attacks, retreating to their barracks during Israeli raids, and sometimes arresting Palestinian gunmen deemed a threat by Israel.
+
+// To supporters, the coordination is a trust-building mechanism that helps stabilize relations with Israel and sets the stage for a Palestinian state. To detractors, including militant groups like Hamas, it is an act of betrayal and acquiescence to Israel that brings Palestinians little benefit, let alone sovereignty.
+
+// After the raid Thursday, the authority announced the suspension of security coordination. If fully established, the move would cut off most contact between the Israeli and Palestinian security services, making it easier for both armed Palestinian groups and violent Israeli settlers to act unimpeded.
+
+// Rising frustration and violence among young Palestinians is also contributing to a combustible situation. The number of deadly Palestinian attacks on Israelis rose last year. So did the level of Palestinian resistance to Israeli military raids, which in turn caused more deadly gunfights between Palestinian armed groups and the Israeli army in the heart of Palestinian cities.
+
+// All of this raises the likelihood of a conflagration and lessens the appeal of cooperation with Israel among the Palestinian leadership and security apparatus.
+
+
+// Analysts and diplomats briefed on the suspension decision believe that at least some level of cooperation with Israel will continue in secret and could be quickly reinstated in full — just as it was in 2017 and 2020.
+
+// But the current context may make it harder than in the past for the authority to reverse its stance, said Ibrahim Dalalsha, the director of the Horizon Center, a Palestinian political research group.
+
+// Tensions with the Israeli government — members of which have openly called for the authority’s collapse — are unlikely to subside fast enough to allow the authority to back down without losing face, Dalalsha said.`,
+//       src: "./images/Img3.png",
+//     },
+//   ];
+
+
+  let article = `<div class="class-articles">
+  <div class="div-right">
+    <div class="article-Topic">
+    <img src="./images/authorPhoto.png" alt="AuthorPhoto">&nbsp;
+      <p class="main-AuthorsName">Authors name&nbsp;</p>
+      <p1 class="word-IN">in&nbsp;</p1>
+      <p2 class="main-Author-Topic">Topics Name&nbsp; &middot; &nbsp;</p2
+      >
+      <p3 class="date">7 July</p3>
+    </div>
+    <a href="./scndPage.html">
+            <div class="article-content">
+              <p class="article-Title"></p>
+              <p class="article-Text"></p>
+            </div>
+          </a>
+    <div class="articleBottom-Info">
+      <div class="article-Topic">
+        <button class="button-JS"> <p class="Article-Bottom-about">Java Script</p></button>
+        <p class="minsToRead">&nbsp;12 min to read&nbsp; &middot; &nbsp;</p>
+        <p class="word-IN">Selected for you</p>
+      </div>
+      <div class="greyIcons">
+        <img src="./images/Icon.png" alt="icon">
+        <img src="./images/Icon.png" alt="icon">
+        <img src="./images/Icon.png" alt="icon">
+      </div>
+    </div>
+  </div>
+  <img class="article-Image" src="" alt="img" />
+
+  </div>
+  <hr>`;
+
+
+  //from Friday lecture:
+  const articles = document.getElementById("articless");
+
+const getTopStories = async () => {
+    const response = await fetch('https://api.nytimes.com/svc/topstories/v2/world.json?api-key=jwVkpVz3S3Kp01xHzzrEs2B6AwuGdwmC')
+    if(response.ok){
+        const articles2 = await response.json();
+        console.log(articles2);
+        articles2.results.splice(0,3).forEach((item) => {
+            let newArticle = article.replace(
+              'class="article-Title">',
+              `class="article-Title">${item.title}`
+            );
+            newArticle = newArticle.replace(
+              'class="article-Text">',
+              `class="article-Text">${item.abstract}`
+            );
+
+            newArticle = newArticle.replace('src=""', `src="${item.multimedia[0].url}"`);
+
+            newArticle = newArticle.replace(
+                'class="main-AuthorsName">Authors name',
+                `class="main-AuthorsName">${item.byline}`
+              );
+
+              newArticle = newArticle.replace(
+                'class="main-Author-Topic">Topics Name',
+                `class="main-Author-Topic">${item.section}`
+              );
+
+              //replacing date
+              newArticle = newArticle.replace(
+                'class="date">7 July',
+                `class="date">${item.created_date.substr(0, 10)}`
+              );
+
+              //replace JavaScript text on the article bottom:
+              newArticle = newArticle.replace(
+                'class="Article-Bottom-about">Java Script',
+                `class="Article-Bottom-about">${item.subsection ? item.subsection : "world"}`
+              );
+
+            articles.innerHTML += newArticle;
+
+        }
+        );
+
+    }else{
+       console.log('запрос составлен неправильно!')
+    }
+  }
+
+  getTopStories();
+
+
+  // below is for te second page
+  let articlePage2 = `<div id="articles-Page2">
+<div class="class-articles">
+<div class="page2-AuthorInfo">
+  <img class="page-2AuthorPhoto" src="./images/authorPhoto2.png" alt="AuthorPhoto" />&nbsp;
+  <div class="page2-Authorinfo-2">
+    <p class="main-AuthorsName">&nbsp;</p>
+    <div class="page2-Authorinfo-3">
+      <p3 class="date">7 July</p3>
+      <p class="minsToRead">&nbsp;&middot; &nbsp;12 min to read&nbsp; &middot; &nbsp;</p>
+      <p class="minsToRead">Member-only</p>
+    </div>
+  </div>
+</div>
+<div class="page2-Logos">
+  <img src="./images/IconLnkdIn.png" alt="Linkedin-Logo">
+  <img src="./images/IconFB.png" alt="FB-Logo">
+  <img src="./images/IconTW.png" alt="Twitter-Logo">
+</div>
+</div>
+<div class="page2-article">
+  <p class="article-Title"></p>
+  <p class="article-Text"></p>
+</div>
+<img src="" alt="picPage2" class="page2-imageBig">
+<div class="page2-mainArticle">
+  <p class="page2-Subheader">Subheader</p>
+  <p class="page2-Article-Content"></p>
+</div>
+<div class="page2-Discussions">
+  <div class="page-2Discussions-ETC">
+      <div class="likesAndDisc-counter">
+      <img src="./images/Heart.png" alt="Likes-Icon">
+      <p class="minsToRead">&nbsp;108</p>
+  </div>
+  <div class="likesAndDisc-counter">
+      <img src="./images/SpeechBubble.png" alt="Discussions-Icon">
+      <p class="minsToRead">&nbsp;18</p>
+  </div>
+  </div>
+  <button class="returnFrom-ScndPage">
+      <img src="./images/Bookmark.png" alt="bookmark">
+  </button>
+</div>
+<hr>
+</div>`
+
+const page22 = document.getElementById("container2");
+
+const getTopStories2 = async () => {
+  const response = await fetch('https://api.nytimes.com/svc/topstories/v2/world.json?api-key=jwVkpVz3S3Kp01xHzzrEs2B6AwuGdwmC')
+  if(response.ok){
+      const articles3 = await response.json();
+      console.log(articles3);
+      articles3.results.splice(0, 1).forEach((item) => {
+
+          let newArticle2 = articlePage2.replace(
+            'class="article-Title">',
+            `class="article-Title">${item.title}`
+          );
+          newArticle2 = newArticle2.replace(
+            'class="article-Text">',
+            `class="article-Text">${item.abstract}`
+          );
+
+          newArticle2 = newArticle2.replace('src="" alt="picPage2"', `src="${item.multimedia[0].url}" alt="picPage2"`);
+
+          newArticle2 = newArticle2.replace(
+              'class="main-AuthorsName">',
+              `class="main-AuthorsName">${item.byline}`
+            );
+
+            newArticle2 = newArticle2.replace(
+                'class="page2-Article-Content">',
+                    `class="page2-Article-Content">${item.abstract}`
+                  );
+
+                  newArticle2 = newArticle2.replace(
+                    'class="date">7 July',
+                    `class="date">${item.created_date.substr(0, 10)}`
+                  );
+
+          page22.innerHTML += newArticle2;
+
+      });
+  }else{
+     console.log('запрос составлен неправильно!')
+  }
+}
+
+getTopStories2();
